94c20f80b56de1147254d3dd718181460417cf86
roattes <roattes@gmail.com>
2025-11-05 10:56:44 -0500

    Basic period CRUD

---
+++ app.py
@@ -0,0 +1,78 @@
+import datetime
+import calendar
+import streamlit as st
+
+from database import get_query, run_query, init_schema, add_period, check_period_exists, get_periods, delete_period
+
+st.set_page_config(layout="wide")
+
+#########################
+# State Management
+#########################
+
+if "init" not in st.session_state:
+    st.session_state["init"] = True
+    init_schema()
+
+selected_user = st.selectbox("Pick a user", ("Lachlan", "Ryan", "Amelia", "Andrea"))
+selected_year = st.selectbox("Pick a year", (2025, 2026, 2027), index=0)
+selected_month = st.selectbox("Pick a month", (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12), index=datetime.datetime.now().month - 1)
+
+
+col1, col2, col3 = st.columns([1, 1, 1])
+with col2:
+#########################
+# Data Entry Form
+#########################
+
+    min_date = datetime.date(selected_year, selected_month, 1)
+    _, month_days = calendar.monthrange(selected_year, selected_month)
+    max_date = datetime.date(selected_year, selected_month, month_days)
+
+    st.write("Add dates you aren't available")
+
+    selected_date = st.date_input("Pick a date", None, min_value=min_date, max_value=max_date)
+    selected_slot = st.selectbox("Pick a time slot", ("Morning", "Afternoon", "All Day"), index=2)
+
+    selected_period = {
+        "date": selected_date, 
+        "slot": selected_slot, 
+        "user": selected_user,
+        "year": selected_year,
+        "month": selected_month
+        }
+
+    select_now = st.button("Add a Period")
+    if select_now:
+        if selected_period["date"] is not None:
+            #st.session_state["period_list"].append(selected_period)
+            if not check_period_exists(selected_period):
+                add_period(selected_period)
+
+#########################
+# Selected Period Output
+#########################
+with col1:
+    #for period in st.session_state["period_list"]:
+    for period in get_periods(selected_user, selected_year, selected_month):
+        st.write(period["date"], period["slot"])
+
+
+with col3:
+    existing_periods = get_periods(selected_user, selected_year, selected_month)
+    
+    deletable_items = []
+    for p in existing_periods:
+        deletable_items.append(p["date"].strftime('%Y-%m-%d') + " " + p["slot"])
+    
+    item_to_delete = st.selectbox("Pick a period", deletable_items)
+
+    index_to_delete = deletable_items.index(item_to_delete)
+
+    period_to_delete = existing_periods[index_to_delete]
+
+    delete_now = st.button("Delete")
+    if delete_now:
+        #print(period_to_delete)
+        delete_period(period_to_delete)
+        st.rerun()
---
+++ database.py
@@ -0,0 +1,83 @@
+import psycopg2
+import psycopg2.extras
+
+POSTGRES_PASSWORD = "lachlan"
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="postgres",
+    user="postgres",
+    password=POSTGRES_PASSWORD,
+    port="5432"  # Default PostgreSQL port
+)
+
+def run_query(query_sql):
+    cur = conn.cursor()
+    cur.execute(query_sql)
+    conn.commit()
+
+def get_query(query_sql):
+    cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+    cur.execute(query_sql)
+    rows = cur.fetchall()
+    return rows
+
+def init_schema():
+    run_query("""
+              CREATE SCHEMA IF NOT EXISTS doc_sched
+              """)
+    run_query("""
+              CREATE TABLE IF NOT EXISTS doc_sched.periods (
+              user_name TEXT,
+              year INT,
+              month INT,
+              date DATE,
+              slot TEXT
+              )
+              """)
+
+def check_period_exists(period):
+    query_sql = f"""
+        SELECT user_name, year, month, date, slot
+        FROM doc_sched.periods
+        WHERE user_name='{period["user"]}'
+        AND year={period["year"]}
+        AND month={period["month"]}
+        AND date='{period["date"]}'
+        AND slot='{period["slot"]}'
+        """
+    results = get_query(query_sql)
+    if len(results) == 0:
+        print("Period Not Found")
+        return False
+    else:
+        print("Period Exists")
+        return True
+    
+def add_period(period):
+    query_sql = f"""
+        INSERT INTO doc_sched.periods (user_name, year, month, date, slot)
+        VALUES ('{period["user"]}', {period["year"]}, {period["month"]}, '{period["date"]}', '{period["slot"]}')
+        """
+    run_query(query_sql)
+
+def get_periods(user, year, month):
+    query_sql = f"""
+        SELECT user_name, year, month, date, slot
+        FROM doc_sched.periods
+        WHERE user_name='{user}'
+        AND year={year}
+        AND month={month}
+        """
+    return get_query(query_sql)
+
+def delete_period(period):
+    query_sql = f"""
+        DELETE FROM doc_sched.periods
+        WHERE user_name='{period["user_name"]}'
+        AND year={period["year"]}
+        AND month={period["month"]}
+        AND date='{period["date"]}'
+        AND slot='{period["slot"]}'
+        """
+    run_query(query_sql)
